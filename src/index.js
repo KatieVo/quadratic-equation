ccfd4917ab57194c3744c6fa4bbeef1043b04276
Kate <ekaterinavolkovaalex@gmail.com>
2018-02-21 14:21:08 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,31 +1,10 @@
 module.exports = function solveEquation(equation) {
     var result = [];
-
-  equation = equation.replace(/\s+/g, '');
-  var myRe = /(-|\+)\d+/g;
-  var found = equation.match(myRe);
-  if (found.length < 3) {
-      var a = parseInt(equation, 10);
-      var b = found[0];
-      var c = found[1];
-      return getSolution(a, b, c);
-  } else {
-      var a = found[0];
-      var b = found[1];
-      var c = found[2];
-      return getSolution(a, b, c);
-  }
-  return result;
-
-  function countDesc(a, b, c) {
-      return b*b - 4*a*c;
-  }
-
-  function getSolution(a, b, c) {
-      var desc = countDesc(a, b, c);
-      var x1 = Math.round((-1*b + Math.sqrt(desc))/(2*a));
-      var x2 = Math.round((-1*b - Math.sqrt(desc))/(2*a));
-      (x1 < x2) ? result.push(x1, x2) : result.push(x2, x1);
-      return result;
-  }
+  equation = equation.replace(/\s+/g, '').match(/(\+|-)?\d+/g);
+  equation.splice(1, 1);
+  var disc = Math.pow(equation[1], 2) - 4*equation[0]*equation[2];
+  var x1 = Math.round((-1*equation[1] + Math.sqrt(disc))/(2*equation[0]));
+  var x2 = Math.round((-1*equation[1] - Math.sqrt(disc))/(2*equation[0]));
+  (x1 < x2) ? result.push(x1, x2) : result.push(x2, x1);
+   return result;
 }
